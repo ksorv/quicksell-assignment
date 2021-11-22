90c12cbbedd7f317d33e029545d49a378286ed0e
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-11-22 19:41:01 +0530

    [feat]: use useApi hook to get initial value for counter
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useEffect } from 'react'
 import Counter from './Counter'
+import { useApi } from './hooks'
+import { getEndpoint } from './utils'
+import { API_URL, ENDPOINT_NAME } from './constants'
 import './styles.css'
 
 export default function App() {
+  const { loading, error, data, call } = useApi()
+
+  useEffect(() => {
+    call({
+      url: getEndpoint(
+        import.meta.env[API_URL],
+        import.meta.env[ENDPOINT_NAME]
+      ),
+    })
+  }, [])
+
+  if (error) {
+    return (
+      <div className="app flex center">
+        <h1>OOPS! Something bad happened!</h1>
+      </div>
+    )
+  }
+
+  if (loading) {
+    return (
+      <div className="app flex center">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="app flex center">
-      <Counter />
+      <Counter initialValue={data} />
     </div>
   )
 }
