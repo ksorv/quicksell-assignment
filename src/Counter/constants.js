3e61532f066fb71aa0c6e2fc26e629218110df8d
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-11-22 20:01:50 +0530

    add Max value env var and move constants around
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- src/Counter/constants.js
+++ src/Counter/constants.js
@@ -8,3 +8,6 @@ export const states = {
   LOADING: 'loading',
   ERROR: 'error',
 }
+
+export const SAVE_TIME = import.meta.env.VITE_SAVE_TIME
+export const MAX_VALUE = import.meta.env.VITE_MAX_VALUE
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const ENDPOINT_NAME = import.meta.env.VITE_NAME
-export const SAVE_TIME = import.meta.env.VITE_SAVE_TIME
